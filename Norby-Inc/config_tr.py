cd9930fff13cdfe532768b1a6ee395b391e6838f
Kanayo Denis-Akano <kanayodenis@gmail.com>
2023-10-19 02:54:42 +0100

    Delete Norby-Inc/config_tr.py

--- Norby-Inc/config_tr.py
+++
@@ -1,8 +0,0 @@
-# Configuration settings
-config_tr = {
-    "database_url": "postgresql://c5gp015210:rYm1NiK0qt@34.89.230.185:5432/d2b_accessment",
-    "script_directory": "C:\Users\ASUS\Desktop\projects\capstone_project\ELT_pipeline_norbyInc_analytics\sql_scripts",
-}
-
-def get_config_tr():
-    return config_tr
